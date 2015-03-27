0010bef71ed72cbe7459cfede3655e04ab5cac01
Steve Popp <spopp@navisite.com>
2015-03-26 23:45:09 -0400

    Added Song Form

--- client/app/song/song.controller.js
+++ client/app/song/song.controller.js
@@ -1,6 +1,10 @@
 'use strict';
 
 angular.module('musicappApp')
-  .controller('SongCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('SongCtrl', function ($scope, $http, $scope) {
+  	$scope.songs = [];
+
+    $http.get('/api/song').success(function(songs) {
+      $scope.songs = songs;
+    });
   });
