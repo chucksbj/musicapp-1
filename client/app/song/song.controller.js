cf0b4d1257eb56c5204dd452e820058ab62b4ea7
Steve Popp <spopp@navisite.com>
2015-05-02 21:28:20 -0400

    Delete song button added

--- client/app/song/song.controller.js
+++ client/app/song/song.controller.js
@@ -1,26 +1,27 @@
 'use strict';
 
 angular.module('musicappApp')
-  .controller('SongCtrl', function ($scope, $http, socket) {
+  .controller('SongCtrl', function ($scope, $http, $log, socket) {
+
     $scope.song = {};
     $scope.songs = [];
     $scope.showForm = false;
 
-    $scope.edit = function edit(row){
-      console.log("Here I need to know which button was selected " + row.entity.name);
-    };
 
     $scope.gridOptions = {
         enableSorting: true,
         enableFiltering: true,
-        columnDefs: [ 
+        multiSelect: false,
+        columnDefs: [
+          { field: '_id', visible: false },
           { name: 'title', displayName: 'Title', enableSorting: true, enableFiltering: true },
           { name: 'words', displayName: 'Words', enableSorting: true, enableFiltering: true },
-          { name: 'startKey', displayName: 'Start Key', enableSorting: true, enableFiltering: true },
-          { name: 'auxKey', displayName: 'Aux Key',  enableSorting: false, enableFiltering: false },
-          { field: '_id', visible: false },
-          { field: '__V', visible: false },
-          {name: 'edit', displayName: 'Edit', enableSorting: false, enableFiltering: false, cellTemplate: '<button id="editBtn" type="button" class="btn btn-primary" ng-click="edit(row)">Edit</button>'}
+          { name: 'startKey', displayName: 'Start Key', width: 80, enableSorting: true, enableFiltering: true },
+          { name: 'auxKey', displayName: 'Aux Key',  width: 80, enableSorting: false, enableFiltering: false },
+          
+          //{ field: '__V', visible: false },
+          {name: 'edit', displayName: 'Edit', width: 65, enableSorting: false, enableFiltering: false, cellTemplate: '<button id="editBtn" type="button" class="btn btn-small" ng-click="grid.appScope.editSong(row.entity) ">Edit</button>'},
+          {name: 'delete', displayName: 'Delete', width: 65, enableSorting: false, enableFiltering: false, cellTemplate: '<button id="deleteBtn" type="button" class="btn btn-small" ng-click="grid.appScope.deleteSong(row.entity)" >Delete</button> '}
         ]
       };
 
@@ -50,15 +51,14 @@ angular.module('musicappApp')
       $scope.song = '';
     };
 
+    $scope.editSong = function(entity) {
+      $log.debug('Which button was selected ' + entity.title);
+    };
+
     $scope.deleteSong = function(song) {
       $http.delete('/api/songs/' + song._id);
     };
 
-    $scope.clickHandler = {
-      onClick: function(value) {
-        window.alert('Name: '+value);
-      }
-    };
 
     $scope.$on('$destroy', function () {
       socket.unsyncUpdates('song');
