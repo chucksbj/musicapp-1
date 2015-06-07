7617027e4e1a9139641b38f43b9cf1f80aef1dc5
Steve Popp <spopp@navisite.com>
2015-06-07 17:09:00 -0400

    More changes for auto next song selection

--- client/app/sheetMusicDisplay/sheetMusicDisplay.controller.js
+++ client/app/sheetMusicDisplay/sheetMusicDisplay.controller.js
@@ -1,9 +1,19 @@
 'use strict';
 
 angular.module('musicappApp')
-  .controller('SheetMusicDisplayCtrl', function ($scope, $http, $rootScope) {
+  .controller('SheetMusicDisplayCtrl', function ($scope, $http, socket, $rootScope) {
     $scope.sheetMusicDisplay = {};
     $scope.sheetMusicDisplays = [];
     $scope.instrumentSelect = $rootScope.instrumentSelect;
     $scope.songSelect = $rootScope.songSelect;
+
+    $http.get('/api/things').success(function(awesomeThings) {
+      $scope.awesomeThings = awesomeThings;
+      socket.syncUpdates('thing', $scope.awesomeThings);
+    });
+
+    $scope.currentSongSelected = function() {
+        $scope.songSelect = $scope.awesomeThings[$scope.awesomeThings.length - 1];
+    };
+
   });
